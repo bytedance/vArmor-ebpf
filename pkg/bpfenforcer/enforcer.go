5598417d70a7539ec64456fe95758040e95d0c1f
weiwei.danny <weiwei.danny@bytedance.com>
2023-11-24 16:55:56 +0800

    fix: remove useless code

--- pkg/bpfenforcer/enforcer.go
+++ pkg/bpfenforcer/enforcer.go
@@ -15,8 +15,6 @@
 
 package bpfenforcer
 
-import "C"
-
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS -target bpfel bpf bpf/enforcer.c -- -I./bpf/headers
 import (
 	"fmt"
