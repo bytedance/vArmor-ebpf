ea4b31d014ba16067c63cfcee6139b9025b8cea7
Danny__Wei <weiwei.danny@bytedance.com>
2026-05-25 21:14:26 +0800

    Merge pull request #27 from bytedance/release-0.4.2
    Update maximun count of rules for policy primitives

--- pkg/bpfenforcer/const.go
+++ pkg/bpfenforcer/const.go
@@ -21,10 +21,10 @@ import (
 
 const (
 	// The maximum count of rules for policy primitives.
-	MaxBpfFileRuleCount    = 50
-	MaxBpfBprmRuleCount    = 50
-	MaxBpfNetworkRuleCount = 50
-	MaxBpfMountRuleCount   = 50
+	MaxBpfFileRuleCount    = 64
+	MaxBpfBprmRuleCount    = 64
+	MaxBpfNetworkRuleCount = 64
+	MaxBpfMountRuleCount   = 64
 
 	// MaxFilePathPatternLength is the maximum length of path pattern,
 	// it's equal to FILE_PATH_PATTERN_SIZE_MAX in BPF code
