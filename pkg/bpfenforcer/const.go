31e3187f30ef2afbf934aeb8044d286911731fa3
Danny__Wei <schindler36@live.com>
2026-05-25 21:01:04 +0800

    Revert "refactor: Increase inner map max entries from 50 to 128 (#24)"
    This reverts commit 8b8db7a70cf45f5f74e339b82cefed3968918cab.

--- pkg/bpfenforcer/const.go
+++ pkg/bpfenforcer/const.go
@@ -21,10 +21,10 @@ import (
 
 const (
 	// The maximum count of rules for policy primitives.
-	MaxBpfFileRuleCount    = 128
-	MaxBpfBprmRuleCount    = 128
-	MaxBpfNetworkRuleCount = 128
-	MaxBpfMountRuleCount   = 128
+	MaxBpfFileRuleCount    = 50
+	MaxBpfBprmRuleCount    = 50
+	MaxBpfNetworkRuleCount = 50
+	MaxBpfMountRuleCount   = 50
 
 	// MaxFilePathPatternLength is the maximum length of path pattern,
 	// it's equal to FILE_PATH_PATTERN_SIZE_MAX in BPF code
